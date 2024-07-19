eb6c76bb5193890c5904687ff8e689c7ad6b90a1
Khalif Gaffarezka <kgaffarezka@gmail.com>
2024-07-19 15:31:39 +0700

    Khalif Edit Error

--- src/pages/EnterPin.tsx
+++ src/pages/EnterPin.tsx
@@ -4,22 +4,9 @@ import enterPinLogo from "../assets/images/enterpin.webp";
 
 // import Input from "../components/Input";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import InputPin from "../components/InputPin";
 
 function EnterPin() {
-  const [pin, setPin] = useState<string[]>(new Array(5).fill(""));
-
-  const handleChange = (value: string, index: number) => {
-    const newPin = [...pin];
-    newPin[index] = value;
-    setPin(newPin);
-
-    // Automatically focus on the next input field
-    if (value && index < pin.length - 1) {
-      (document.getElementById(`pin-${index + 1}`) as HTMLInputElement).focus();
-    }
-  };
-
   return (
     <main className="font-montserrat grid place-items-center md:grid-cols-2 bg-bgprimary h-screen">
       <section className="bg-white rounded-3xl w-4/5 tbt:w-96 md:w-full md:h-full md:rounded-r-3xl md:rounded-l-none px-5 md:px-20 py-10 md:py-52">
@@ -34,19 +21,7 @@ function EnterPin() {
           <p className="text-gray-500 text-xs">Please save your pin because this so important.</p>
         </div>
         <form className="mt-5">
-          <div className="flex justify-between mb-6">
-            {pin.map((digit, index) => (
-              <input
-                key={index}
-                id={`pin-${index}`}
-                type="text"
-                maxLength={1}
-                className="w-7 h-10 py-5 text-center font-semibold text-lg border-b-2 border-gray-300 focus:outline-none focus:border-blue-500"
-                value={digit}
-                onChange={(e) => handleChange(e.target.value, index)}
-              />
-            ))}
-          </div>
+          <InputPin length={5} />
           <button className="text-white mt-3 uw:text-2xl bg-primary hover:bg-blue-700 active:bg-blue-800 rounded-lg w-full h-11 uw:h-16" type="submit">
             Submit
           </button>

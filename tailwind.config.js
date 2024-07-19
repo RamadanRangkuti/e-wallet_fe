90923f3235c4ce8b660f95c04b363bc25a5d0c9b
IrmawanAriel <158867264+IrmawanAriel@users.noreply.github.com>
2024-07-19 14:51:59 +0700

    Revert "Ariel dashboard"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-const tailwindConfig = {
+export default {
   content: ["./src/**/*.tsx"],
   theme: {
-    screens: {
-      xxxsm: "300px",
-      xxsm: "321px",
-      xsm: "376px",
-      tbt: "426px",
-      sm: "641px",
-      md: "769px",
-      lg: "1025px",
-      xl: "1281px",
-      "2xl": "1441px",
-      "4xl": "1537px",
-      uw: "2041px",
-    },
-    extend: {
-      fontFamily: {
-        montserrat: ["Montserrat", "sans-serif"],
-        nunitosans: ["Nunito Sans", "sans-serif"],
-      },
-      colors: {
-        primary: "#2948FF",
-        bgprimary: "#3969FD",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
-
-export default tailwindConfig;

211b70fcd83747985dd94bfcbeefb623134b9407
Khalif Gaffarezka <kgaffarezka@gmail.com>
2024-07-23 20:12:23 +0700

    edit axios register

--- src/pages/TransferHistory.tsx
+++ src/pages/TransferHistory.tsx
@@ -45,17 +45,16 @@ function TransferHistory() {
       <div className="flex items-center bg-primary w-screen text-white text-xs md:text-base md:font-bold py-2 px-5 md:px-8 md:bg-white md:text-black  md:hidden">
         <div>History Transaction</div>
       </div>
-      <div className="bg-white w-full p-4">
+      <div className="bg-white w-full md:p-4">
         <div className="w-full max-md:max-w-full ">
           <div className="flex gap-5 max-md:flex-col max-md:gap-0">
-
-            <div className="flex flex-col ml-5 w-4/5 max-md:ml-0 max-md:w-full w-ful">
-              <div className="flex flex-col  max-md:max-w-full p-4 w-full">
-                <div className="flex gap-4 self-start px-5 font-semibold tracking-normal leading-6 text-slate-900 text-lg hidden md:flex">
+            <div className="flex flex-col ml-5 w-4/5 max-md:ml-0 max-md:w-full">
+              <div className="flex flex-col  max-md:max-w-full md:p-4 w-full">
+                <div className="gap-4 self-start px-5 font-semibold tracking-normal leading-6 text-slate-900 text-lg hidden md:flex">
                   <img loading="lazy" src={historyTransfer} className="shrink-0 my-auto w-6 aspect-square" alt="History Transfer" />
                   <div>History Transaction</div>
                 </div>
-                <div className="flex flex-col px-8 pt-6 pb-11 mt-9 border border-gray-200 border-solid max-md:px-5 max-md:max-w-full p-8 w-full gap-4">
+                <div className="flex flex-col px-8 pt-6 pb-11 md:mt-9 md:border md:border-gray-200 md:border-solid max-md:px-5 max-md:max-w-full p-8 w-full gap-4">
                   <div className="flex gap-5 w-full max-md:flex-wrap max-md:pr-5 max-md:max-w-full">
                     <div className="flex-auto my-auto font-semibold tracking-normal leading-6 text-slate-900 text-lg">Find Transaction</div>
                     <div className="flex gap-8 p-4 text-sm font-medium leading-5 text-gray-600 rounded-md border border-gray-200 border-solid">
@@ -84,7 +83,8 @@ function TransferHistory() {
             </div>
           </div>
         </div>
-      </div></>
+      </div>
+    </>
   );
 }
 

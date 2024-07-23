4f22d499074d4cf4d41a7b91603b1ea984f2d8d1
arielirmawan <arielirmawan@gmail.com>
2024-07-24 06:41:57 +0700

    tetrakhir hari ini

--- src/components/TransactionHistory.tsx
+++ src/components/TransactionHistory.tsx
@@ -10,7 +10,7 @@ interface Transaction {
   type: string;
   transfer_amount: string;
   isIncome: boolean;
-  imageUrl: string;
+  sender_image: string;
 }
 
 const TransactionHistory = () => {
@@ -46,7 +46,7 @@ const TransactionHistory = () => {
       </div>
       {transactions.map((transaction) => (
         <div key={transaction.id} className="flex gap-5 justify-between items-center mt-7 text-base p-4">
-          <img loading="lazy" src={transaction.imageUrl} className="shrink-0 self-stretch w-14 aspect-square" alt={`${transaction.sender_fullname}'s profile`} />
+          <img loading="lazy" src={transaction.sender_image} className="shrink-0 self-stretch w-14 aspect-square" alt={`${transaction.sender_fullname}'s profile`} />
           <div className="flex flex-col self-stretch pr-2.5 my-auto">
             <div className="font-semibold text-slate-900">{transaction.sender_fullname}</div>
             <div className="mt-3 text-gray-600">{transaction.type}</div>

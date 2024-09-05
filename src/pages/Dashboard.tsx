2a6fc5c4976aec9441a38ad7212f31dab10bc037
arielirmawan <arielirmawan@gmail.com>
2024-09-05 20:10:31 +0700

    Last revision (-pagination)

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,14 +1,11 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-// import TransactionHistory from "../components/TransactionHistory";
 import transfer from "../assets/icons/transferLight.svg";
 import topUp from "../assets/icons/topUpLight.svg";
 import balanceIcon from "../assets/icons/balance.svg";
 import Upgraph from "../assets/icons/Upgraph.svg";
 import Downgraph from "../assets/icons/Downgraph.svg";
 import user from "../assets/images/user.webp";
-// import profile from "../assets/images/ProfileNavbar.png";
-// import profileImage from "../assets/images/user.webp";
 import FinancialChart from "../components/FinancialChart";
 import { Link } from "react-router-dom";
 import { useStoreSelector } from "../redux/hooks";
